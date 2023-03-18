b9d48d4fd8250afbc94982aef8a99c2a2869b7b0
zlx <1492295894@qq.com>
2023-03-18 18:01:06 +0800

    feat: add router "/github/webhook"

--- router.go
+++ router.go
@@ -10,6 +10,6 @@ import (
 // customizeRegister registers customize routers.
 func customizedRegister(r *server.Hertz) {
 	r.GET("/ping", handler.Ping)
-
+	r.POST("/github/webhook", handler.WebHook)
 	// your code ...
 }
